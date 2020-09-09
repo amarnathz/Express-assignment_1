8cc602507b0b2c4c8a15543feca826f5e466da19
Amarnath <36194961+amarnathz@users.noreply.github.com>
2020-09-09 15:48:45 +0530

    Add files via upload

---
+++ routes/leaderRouter.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const leaderRouter = express.Router();
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader("content-type", "text/plain");
+        next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send all the leaders to you!');
+    })
+    .post((req, res, next) => {
+        res.end('Will add the leader : ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /leaders');
+    })
+    .delete((req, res, next) => {
+        res.end('Deleting all leaders');
+    });
+
+leaderRouter.route('/:leadersId')
+    .get((req, res, next) => {
+        res.end('Will send details of the leaders: ' + req.params.leadersId + ' to you!');
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /leaders/' + req.params.leadersId);
+    })
+    .put((req, res, next) => {
+        res.write('Updating the leaders: ' + req.params.leadersId + '\n');
+        res.end('Will update the leaders: ' + req.body.name +
+            ' with details: ' + req.body.description);
+    })
+    .delete((req, res, next) => {
+        res.end('Deleting leaders: ' + req.params.leadersId);
+    });
+
+module.exports = leaderRouter;
